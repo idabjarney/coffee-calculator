eb7b2a845ed79ff6b496189ac061131679e90061
idaism <ida.bjarney@outlook.com>
2024-01-09 19:59:42 +0000

    finished algoritm for calculator

--- src/main.ts
+++ src/main.ts
@@ -4,60 +4,111 @@ const inputContainers: NodeList = document.querySelectorAll('.input');
 const ageInput: HTMLInputElement = document.querySelector('.age-input');
 const cupsPerWeekInput: HTMLInputElement = document.querySelector('.cups-per-week-input');
 const coffePriceInput: HTMLInputElement = document.querySelector('.coffee-price-input');
-const submitBtn: HTMLInputElement = document.querySelector('.submit-btn');
+const form: HTMLFormElement = document.querySelector('form');
+const tryAgainBtn: HTMLButtonElement = document.querySelector('.try-again-btn');
+const resultsWrapper: HTMLDivElement = document.querySelector('.results-wrapper');
+const calculatorForm: HTMLFormElement = document.querySelector('.calculator-form');
+const tabsWrapper: HTMLDivElement = document.querySelector('.tabs-wrapper');
+const inputContainer: HTMLDivElement = document.querySelector('.input-container');
+const calculationInfoTab: HTMLDivElement = document.querySelector('.calculation-info-tab');
+const buttonWrapper: HTMLDivElement = document.querySelector('.button-wrapper');
+const tabs: HTMLDivElement[] = Array.from(document.querySelectorAll('.tab'));
 
 const interestRate: number = 0.0284; // percentage
-const pensionRate: number = 0.02; // percentage
 const inflationRate: number = 0.024; // percentage
 const pensionAge: number = 67;
 const weeks: number = 52;
-const yearPlaceholder: number = 5;
-const fiveYears: number = 5;
-const tenYears: number = 10;
-const fifteenYears: number = 15;
-let cupsPerWeek: number = 0;
-let userAge: number = 0;
-let coffeeCupPrice: number = 0; // £
-const annualNumberOfCups: number = weeks * cupsPerWeek;
-const yearsTillPension: number = calculateYearsUntilPension(userAge, pensionAge);
-const annualPriceOfCups: number = (annualNumberOfCups * coffeeCupPrice);
+let cupsPerWeek: number;
+let userAge: number;
+let coffeeCupPrice: number ; // £
 
-submitBtn.addEventListener('click', (e) => {
+
+const annualNumberOfCups: number = weeks * cupsPerWeek;
+
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let userAge = ageInput.value;
-  console.log(userAge);
+  const [weeklyCostNode, cupPriceNode, annualCostNode]: any = Array.from(document.querySelectorAll('.user-input-value'));
+  userAge = Number(ageInput.value);
+  cupsPerWeek = Number(cupsPerWeekInput.value);
+  coffeeCupPrice = Number(coffePriceInput.value);
+  
+  const costPerWeek  = cupsPerWeek * coffeeCupPrice;
+  const costPerYear = (cupsPerWeek * 52) * coffeeCupPrice;
+
+
+  weeklyCostNode.textContent = costPerWeek;
+  cupPriceNode.textContent = coffeeCupPrice;
+  annualCostNode.textContent = costPerYear.toLocaleString();
+  
+  buttonWrapper.classList.remove('hidden');
+  calculatorForm.classList.add('hidden');
+  resultsWrapper.classList.remove('hidden');
+  calculationInfoTab.classList.remove('hidden');
+  tabsWrapper.classList.remove('hidden');
+  tabsWrapper.classList.remove('hidden');
+  inputContainer.classList.add('border');
 })
 
+tryAgainBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  calculatorForm.classList.remove('hidden');
+  resultsWrapper.classList.add('hidden');
+  calculationInfoTab.classList.add('hidden');
+  tabsWrapper.classList.add('hidden');
+  inputContainer.classList.remove('border');
+  buttonWrapper.classList.add('hidden');
+  changeTab(document.querySelector('.tab-info'));
+})
+
+for(let tab of tabs) {
+  tab.addEventListener('click', (e) => changeTab((e.target as HTMLElement)))
+}
+ 
+function changeTab(tab: HTMLElement) {
+  
+  const activeContent = document.querySelector('.active');
+  if (activeContent) {
+    activeContent.classList.remove('active');
+    activeContent.classList.add('hidden');
+  };
+
+  const showTab = tab.dataset?.showtab;
+  const newActiveTab = document.querySelector(`.${showTab}`);
+  if (newActiveTab) {
+    newActiveTab.classList.remove('hidden');
+    newActiveTab.classList.add('active');
+  }
+
+  const activeTab = document.querySelector('.selected-tab');
+  if (activeTab) {
+    activeTab.classList.remove('selected-tab');
+    tab.classList.add('selected-tab');
+  }
+
+  if (showTab === 'x-year-results') {
+    const years = tab.dataset?.years;
+    const savings = generateSavings(years === 'retirement' ? calculateYearsUntilPension(userAge, pensionAge) : +years);
+    document.querySelector('.x-year').textContent = (years === 'retirement' ? calculateYearsUntilPension(userAge, pensionAge) : +years).toString();
+    document.querySelector('.x-year-savings-result').textContent = savings.toLocaleString();
+  }
+
+
+}
+
+function generateSavings(years): number {
+  let balance = 0;
+  let pricePerCup = coffeeCupPrice;
+  for (let i = 0; i < years; i++) {
+    const pricePerYear = (cupsPerWeek * 52) * pricePerCup;
+    balance += pricePerYear;
+    balance += balance * interestRate;
+    pricePerCup += pricePerCup * inflationRate;
+  }
+  return Math.round(balance);
+}
 
 function calculateYearsUntilPension(userAge, pensionAge) {
-  let years: number = pensionAge - userAge;
+  const years: number = pensionAge - userAge;
   return years;
 }
-
-
-function calculateAnnualRate(cupsPerWeek, rate) {
-  const annualInterest: number = Number((annualNumberOfCups * rate).toFixed(2));
-  return annualInterest;
-}
-
-calculateAnnualRate(cupsPerWeek, interestRate);
-calculateAnnualRate(cupsPerWeek, pensionRate);
-
-function simpleInterestFormula(principal: number, rate: number, time: number): number {
-  const interest = (principal * rate) * time;
-  // const priceWithInterest = interest + annualPriceOfCups;
-  return +interest.toFixed(2);
-};
-
-
-function calculateTimePeriod(years: number) {
-  const calculatedInterest: number = simpleInterestFormula(annualPriceOfCups, interestRate, yearPlaceholder);
-  const calculatedPension: number = simpleInterestFormula(annualPriceOfCups, pensionRate, yearPlaceholder);
-  const annualPriceWithInterest: number = calculatedInterest + calculatedPension + annualPriceOfCups;
-  const total = years * annualPriceWithInterest;
-  console.log(+total.toFixed(2));
-  return +total.toFixed(2);
-}
-
-calculateTimePeriod(yearsTillPension);
-
